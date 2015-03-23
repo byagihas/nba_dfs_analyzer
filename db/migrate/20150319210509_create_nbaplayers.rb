928e72cf7629caf2b8f40b3734aa127cd1524854
Brandon Yagihashi <yagihashib@gmail.com>
2015-03-23 11:12:26 -0400

    Added methods

--- db/migrate/20150319210509_create_nbaplayers.rb
+++ db/migrate/20150319210509_create_nbaplayers.rb
@@ -6,8 +6,15 @@ class CreateNbaplayers < ActiveRecord::Migration
       t.text :team
       t.integer :avgfanduelpoints
       t.boolean :is_starting_lineup
-
       t.timestamps null: false
     end
+  end
+end
+
+class ChangePositionType < ActiveRecord::Migration
+  def up
+    change column :nbaplayers, :position,
+  end
+  def down
   end
 end
