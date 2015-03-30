4139ccf4e416173ebb6f9e6f6e5689330259dc5e
yagihashib <yagihashib@gmail.com>
2015-03-30 13:12:37 -0400

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323214321) do
+ActiveRecord::Schema.define(version: 20150330150402) do
 
   create_table "nbaplayers", force: :cascade do |t|
     t.text     "name"
-    t.string   "position"
+    t.text     "position"
     t.text     "team"
     t.integer  "avgfanduelpoints"
     t.boolean  "is_starting_lineup"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
